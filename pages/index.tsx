eee381643e3efa3442f8b7c973435c7c5669f13c
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-06-02 18:19:57 +0800

    feat: add more information

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import dynamic from "next/dynamic";
 
 const Dynamic = dynamic(
-    async function () {
-        return Wrapper
-    }
+  async function () {
+    return Wrapper
+  }
 )
 
 const Wrapper = () => {
-    return <div>
-        <button>Hello</button>
-    </div>
+  return <div>
+    <button onClick={() => alert('good morning')}>Hello</button>
+  </div>
 }
 
 export default function Page() {
-    return <Dynamic />
+  return <Dynamic />
 }
